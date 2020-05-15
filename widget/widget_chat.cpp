5a0f5dd08544ee3345fb4c05f827c718b2aea9e8
vmana <vmana@vmana.net>
2020-05-15 23:51:00 +0200

    utf8 chat

--- widget/widget_chat.cpp
+++ widget/widget_chat.cpp
@@ -42,12 +42,15 @@ widget_chat::widget_chat() : wcontainer("chat")
 void widget_chat::add_message(string message)
 {
 	// format msg
-	if (S->p_user) message = "<span class=\"widget_chat_player_name\">" + S->p_user->login + " </span> : " + message;
-	if (substr(message, -1) != "\n") message += "\n";
-	message = str_replace("\n", "<br />\n", message);
+	if (S->p_user) message =
+		"<span class=\"widget_chat_player_name\">"
+		+ S->p_user->login
+		+ " </span> : "
+		+ message
+		+ "<br />\n";
 
 	/* for (int i = 0; i < 30; i++) */
-	chat_lines->setText(chat_lines->text() + message);
+	chat_lines->setText(chat_lines->text() + WString::fromUTF8(message));
 
 	// reset visible & timeout
 	chat_container->setStyleClass("widget_chat ss-container");
