89f0c4c76c164cc64352723d4e58431c32245180
vmana <vmana@vmana.net>
2020-05-16 14:19:07 +0200

    moon arc on canvas button

--- js/function.js
+++ js/function.js
@@ -21,7 +21,8 @@ function init_chat_box(wt_chat_id)
 function init_selector_buttons()
 {
 	var radius_middle = 70; // middle round button
-	var space_middle = 6;
+	var space_middle = 8;
+	var moon_arc = 20;
 	var button_width = 150;
 	var button_height = 50;
 	var button_font = 26;
@@ -30,77 +31,69 @@ function init_selector_buttons()
 	var canvas = document.getElementById('selector_clear_canvas');
 	var context = canvas.getContext('2d');
 	canvas.classList.add('selector_buttons');
-	canvas.width = button_width;
-	canvas.height = button_height;
-	canvas.style.left = 'calc(50% - ' + canvas.width +'px - ' + radius_middle + 'px)';
-	canvas.style.bottom = 'calc(' + radius_middle + 'px - (' + canvas.height + 'px / 2))';
+	canvas.width = button_width + radius_middle;
+	canvas.height = 140;
+	canvas.style.left = 'calc(50% - ' + button_width + 'px - ' + radius_middle + 'px)';
+	// canvas.style.bottom = 'calc(' + radius_middle + 'px - (' + canvas.height + 'px / 2))';
+	canvas.style.bottom = '0px';
 
 	context.fillStyle = '#0a141c';
 	context.beginPath();
 	// left circle
-	context.arc((canvas.height / 2), (canvas.height / 2), (canvas.height / 2), 0, 2 * Math.PI, false);
+	context.arc((button_height / 2), (canvas.height / 2), (button_height / 2), 0, 2 * Math.PI, false);
 	// rectangle button
-	context.fillRect(canvas.height / 2, 0, canvas.width - (canvas.height / 2), canvas.height);
+	context.fillRect(button_height / 2, canvas.height / 2 - button_height / 2, button_width, button_height);
+	// moon arc on the right
+	context.arc(button_width + radius_middle - moon_arc, (canvas.height / 2), radius_middle, 0, 2 * Math.PI, false);
 	context.fill();
+
 	// remove half a circle of the right
 	context.save();
 	context.globalCompositeOperation = 'destination-out';
 	context.beginPath();
 
-	context.arc(canvas.width + radius_middle, (canvas.height / 2), radius_middle + space_middle, 0, 2 * Math.PI, false);
+	context.arc(button_width + radius_middle, (canvas.height / 2), radius_middle + space_middle, 0, 2 * Math.PI, false);
 	context.fill();
 	context.restore();
 
 	// button left text
 	context.font = button_font + 'px sans-serif';
 	context.textAlign = "center";
+	context.textBaseline = "middle";
 	context.fillStyle = '#f9e9b8';
-	context.fillText('CLEAR', button_width/2, button_height/2 + 10);
+	context.fillText('CLEAR', button_width/2, canvas.height/2 + 3);
 
 	// button right
 	canvas = document.getElementById('selector_throw_canvas');
 	context = canvas.getContext('2d');
 	canvas.classList.add('selector_buttons');
-	canvas.width = button_width;
-	canvas.height = button_height;
-	canvas.style.right = 'calc(50% - ' + canvas.width +'px - ' + radius_middle + 'px)';
-	canvas.style.bottom = 'calc(' + radius_middle + 'px - (' + canvas.height + 'px / 2))';
+	canvas.width = button_width + radius_middle;
+	canvas.height = 140;
+	canvas.style.right = 'calc(50% - ' + button_width + 'px - ' + radius_middle + 'px)';
+	canvas.style.bottom = '0px';
 
 	context.fillStyle = '#0a141c';
 	context.beginPath();
 	// right circle
-	context.arc(canvas.width - (canvas.height / 2), (canvas.height / 2), (canvas.height / 2), 0, 2 * Math.PI, false);
+	context.arc(canvas.width - (button_height / 2), (canvas.height / 2), (button_height / 2), 0, 2 * Math.PI, false);
 	// rectangle button
-	context.fillRect(0, 0, canvas.width - (canvas.height / 2), canvas.height);
+	context.fillRect(radius_middle, canvas.height / 2 - button_height / 2, button_width - (button_height / 2), button_height);
+	// moon arc on the left
+	context.arc(moon_arc, (canvas.height / 2), radius_middle, 0, 2 * Math.PI, false);
 	context.fill();
 	// remove half a circle of the left
 	context.save();
 	context.globalCompositeOperation = 'destination-out';
 	context.beginPath();
 
-	context.arc(-radius_middle, (canvas.height / 2), radius_middle + space_middle, 0, 2 * Math.PI, false);
+	context.arc(0, (canvas.height / 2), radius_middle + space_middle, 0, 2 * Math.PI, false);
 	context.fill();
 	context.restore();
 
 	// button right text
 	context.font = button_font + 'px sans-serif';
 	context.textAlign = "center";
+	context.textBaseline = "middle";
 	context.fillStyle = '#f9e9b8';
-	context.fillText('THROW', button_width/2, button_height/2 + 10);
-
-	// // right circle
-	// context.arc(canvas.width - (canvas.height / 2), (canvas.height / 2), (canvas.height / 2), 0, 2 * Math.PI, false);
-	// // rectangle button
-	// context.fillRect(0, 0, canvas.width - (canvas.height / 2), canvas.height);
-	// context.fill();
-	// // remove half a circle of the left
-	// context.save();
-	// context.globalCompositeOperation = 'destination-out';
-	// context.beginPath();
-
-	// var left_radius = 100;
-	// var radius_x = -80;
-
-	// context.arc(radius_x, (canvas.height / 2), left_radius, 0, 2 * Math.PI, false);
-	// context.fill();
+	context.fillText('THROW', radius_middle + button_width/2, canvas.height/2 + 3);
 }
